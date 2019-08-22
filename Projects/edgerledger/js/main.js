8bbf523e0e2a619784939481c18bd829821cb2ec
jisshub <jissmon476@gmial.com>
2019-08-22 21:09:56 +0530

    widescreen view

--- Projects/edgerledger/js/main.js
+++ Projects/edgerledger/js/main.js
@@ -12,28 +12,33 @@ function initMap() {
     const marker = new google.maps.Marker({ position: loc, map: map });
   }
   
-//   // Sticky menu background
-//   window.addEventListener('scroll', function() {
-//     if (window.scrollYAIzaSyA1n5JTf1-sbNndXSx-PX9210RRZIj2Wq4 > 150) {
-//       document.querySelector('#navbar').style.opacity = 0.9;
-//     } else {
-//       document.querySelector('#navbar').style.opacity = 1;
-//     }
-//   });
+   // Sticky menu background
+  // n js there is a window obj, which is the whole browser.
+  // & when v scroll down the browser more than 150 then opacity of navbar is set to 0.9.
+  // and if not scroll more than 150, opacity is set to 1, which means not transparent
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });
   
   
-//   // Smooth Scrolling
-//   $('#navbar a, .btn').on('click', function(event) {
-//     if (this.hash !== '') {
-//       event.preventDefault();
+  // Smooth Scrolling
+  // here v implement smooth scrolling on navbars and .btn class
+
+  $('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
   
-//       const hash = this.hash;
+      const hash = this.hash;
   
-//       $('html, body').animate(
-//         {
-//           scrollTop: $(hash).offset().top - 100
-//         },
-//         800
-//       );
-//     }
-//   });
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
